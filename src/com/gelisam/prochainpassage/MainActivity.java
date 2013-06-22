0725dee92e9ef6d2ada955ac26be88e366ba4828
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-22 13:02:01 -0400

    a field for the radio group

--- src/com/gelisam/prochainpassage/MainActivity.java
+++ src/com/gelisam/prochainpassage/MainActivity.java
@@ -3,12 +3,15 @@ package com.gelisam.prochainpassage;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
 	private TextView dataset_name_view;
 	private ListView passage_list_view;
+	
+	private RadioGroup radiobuttons_view;
 	private RadioButton to_montreal_view;
 	private RadioButton to_roxboro_view;
 
@@ -19,6 +22,8 @@ public class MainActivity extends Activity {
 		
 		dataset_name_view = (TextView) findViewById(R.id.dataset_name);
 		passage_list_view = (ListView) findViewById(R.id.passage_list);
+		
+		radiobuttons_view = (RadioGroup) findViewById(R.id.radiobuttons);
 		to_montreal_view = (RadioButton) findViewById(R.id.to_montreal);
 		to_roxboro_view = (RadioButton) findViewById(R.id.to_roxboro);
 	}

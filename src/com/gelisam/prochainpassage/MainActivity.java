2b3f8705cc70fd8f69d48ceea3c9deee9c47327a
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-22 13:06:13 -0400

    dummy radiobutton listener

--- src/com/gelisam/prochainpassage/MainActivity.java
+++ src/com/gelisam/prochainpassage/MainActivity.java
@@ -1,13 +1,16 @@
 package com.gelisam.prochainpassage;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.app.Activity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements RadioGroup.OnCheckedChangeListener {
+	private static final String LOG_TAG = "MainActivity";
+	
 	private TextView dataset_name_view;
 	private ListView passage_list_view;
 	
@@ -26,6 +29,20 @@ public class MainActivity extends Activity {
 		radiobuttons_view = (RadioGroup) findViewById(R.id.radiobuttons);
 		to_montreal_view = (RadioButton) findViewById(R.id.to_montreal);
 		to_roxboro_view = (RadioButton) findViewById(R.id.to_roxboro);
+		
+		radiobuttons_view.setOnCheckedChangeListener(this);
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		switch (checkedId) {
+		case R.id.to_montreal:
+			Log.d(LOG_TAG, "to montreal");
+			break;
+		case R.id.to_roxboro:
+			Log.d(LOG_TAG, "to roxboro");
+			break;
+		}
 	}
 
 }

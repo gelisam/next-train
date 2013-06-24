f1f0ab4682937396a53e7b2356142ef49d23d91d
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-24 17:38:49 -0400

    Scroll to selection

--- src/com/gelisam/prochainpassage/MainActivity.java
+++ src/com/gelisam/prochainpassage/MainActivity.java
@@ -46,7 +46,9 @@ public class MainActivity extends Activity implements RadioGroup.OnCheckedChange
 		Schedule schedule = googleTransit.scheduleForToday(stop_id);
 		
 		dataset_name_view.setText(schedule.service_name + " schedule");
-		schedule_view.setAdapter(new StringListAdapter<StopTime>(this, schedule.stop_times, ii++));
+		schedule_view.setAdapter(new StringListAdapter<StopTime>(this, schedule.stop_times, ii));
+		schedule_view.setSelection(ii);
+		++ii;
 	}
 
 }

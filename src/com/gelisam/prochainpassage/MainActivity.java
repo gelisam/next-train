16d6f939d58015777113165053d8dc9168e5b0db
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-24 16:17:33 -0400

    Remove unused code

--- src/com/gelisam/prochainpassage/MainActivity.java
+++ src/com/gelisam/prochainpassage/MainActivity.java
@@ -1,26 +1,18 @@
 package com.gelisam.prochainpassage;
 
-import java.util.List;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ListView;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.app.Activity;
 
 public class MainActivity extends Activity implements RadioGroup.OnCheckedChangeListener {
-	private static final String LOG_TAG = "MainActivity";
-	
 	private GoogleTransit googleTransit;
 	
 	private TextView dataset_name_view;
 	private ListView schedule_view;
 	
 	private RadioGroup radiobuttons_view;
-	private RadioButton to_montreal_view;
-	private RadioButton to_roxboro_view;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +25,6 @@ public class MainActivity extends Activity implements RadioGroup.OnCheckedChange
 		schedule_view = (ListView) findViewById(R.id.schedule);
 		
 		radiobuttons_view = (RadioGroup) findViewById(R.id.radiobuttons);
-		to_montreal_view = (RadioButton) findViewById(R.id.to_montreal);
-		to_roxboro_view = (RadioButton) findViewById(R.id.to_roxboro);
-		
 		radiobuttons_view.setOnCheckedChangeListener(this);
 	}
 

c69b1dfe77184ad21ae6d32a5c6520e391038d97
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-25 08:57:33 -0400

    start checked
    avoids a crash when stop_name is null.

--- src/com/gelisam/prochainpassage/MainActivity.java
+++ src/com/gelisam/prochainpassage/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends Activity implements RadioGroup.OnCheckedChange
 		
 		radiobuttons_view = (RadioGroup) findViewById(R.id.radiobuttons);
 		radiobuttons_view.setOnCheckedChangeListener(this);
+		radiobuttons_view.check(R.id.to_montreal);
 	}
 	
 	@Override
